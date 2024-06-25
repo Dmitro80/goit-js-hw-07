ffaecd07936f048135159f48269deb0210134194
Dmitro <kt040880bds2@gmail.com>
2024-06-25 21:48:33 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,40 @@
+const input = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  const initialBoxSize = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${initialBoxSize + i * 10}px`;
+    box.style.height = `${initialBoxSize + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
+
+createBtn.addEventListener("click", () => {
+  const amount = parseInt(input.value);
+
+  if (amount >= 1 && amount <= 100) {
+    destroyBoxes();
+    createBoxes(amount);
+    input.value = "";
+  } else {
+    alert("Please enter a number between 1 and 100!");
+  }
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, "0")}`;
 }
