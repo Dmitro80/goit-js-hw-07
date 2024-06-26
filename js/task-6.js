e599079832a2fb97494726c5c4b1812086f3f26b
Dmitro <kt040880bds2@gmail.com>
2024-06-26 13:37:40 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,40 +1,33 @@
-const input = document.querySelector("input");
-const createBtn = document.querySelector("[data-create]");
-const destroyBtn = document.querySelector("[data-destroy]");
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, "0")}`;
+}
+
+const controls = document.querySelector("#controls");
+const input = controls.querySelector('input[type="number"]');
+const createBtn = controls.querySelector("button[data-create]");
+const destroyBtn = controls.querySelector("button[data-destroy]");
 const boxesContainer = document.querySelector("#boxes");
-
-function createBoxes(amount) {
-  const initialBoxSize = 30;
-
-  for (let i = 0; i < amount; i++) {
-    const box = document.createElement("div");
-    box.style.width = `${initialBoxSize + i * 10}px`;
-    box.style.height = `${initialBoxSize + i * 10}px`;
-    box.style.backgroundColor = getRandomHexColor();
-    boxesContainer.appendChild(box);
-  }
-}
-
-function destroyBoxes() {
-  boxesContainer.innerHTML = "";
-}
 
 createBtn.addEventListener("click", () => {
   const amount = parseInt(input.value);
-
-  if (amount >= 1 && amount <= 100) {
-    destroyBoxes();
+  if (amount > 0 && amount <= 100) {
     createBoxes(amount);
     input.value = "";
   } else {
     alert("Please enter a number between 1 and 100!");
   }
 });
+destroyBtn.addEventListener("click", () => (boxesContainer.innerHTML = ""));
 
-destroyBtn.addEventListener("click", destroyBoxes);
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, "0")}`;
+function createBoxes(amount) {
+  const boxesHTML = [];
+  for (let i = 0; i < amount; i++) {
+    const boxSize = 30 + i * 10;
+    const boxColor = getRandomHexColor();
+    boxesHTML.push(`
+      <div style="width: ${boxSize}px; height: ${boxSize}px; background-color: ${boxColor};"></div>`);
+  }
+  boxesContainer.innerHTML = boxesHTML.join("");
 }
